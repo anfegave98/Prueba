e23f4a5a09c2e09894593b4058afa1491d7b26c7
Mateo Ortiz <mortiz@proximateapps.com>
2018-09-10 00:41:41 -0500

    mateo 10.09.2018 12:40 AM

--- web/basic_views/js/register_admin.js
+++ web/basic_views/js/register_admin.js
@@ -1,39 +1,66 @@
-
-
-function validateStore() {
+function validateAdmin() {
     var name_admin = document.getElementById("name_admin").value;
     var surname_addmin = document.getElementById("surname_addmin").value;
     var email_admin = document.getElementById("email_admin").value;
     var pass_admin = document.getElementById("pass_admin").value;
     var pass_2_admin = document.getElementById("pass_2_admin").value;
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+    var emailValues = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if ((name_admin != "") && (surname_addmin != "") && (email_admin != "") && (pass_admin != "") && (pass_2_admin != "")) {
+        if (pass_admin.match(lowerCaseLetters)) {
+            if (pass_admin.match(upperCaseLetters)) {
+                if (pass_admin.match(numbers)) {
+                    if (pass_admin.length >= 8) {
+                        if (email_admin.match(emailValues)) {
+                            if (pass_admin == pass_2_admin) {
+                                var parametros = {
+                                    "op": "create",
+                                    "name": name_admin,
+                                    "password": pass_admin,
+                                    "last_name": surname_addmin,
+                                    "email": email_admin
+                                }
+                                $.ajax({
+                                    data: parametros,
+                                    url: "../../ClientS",
+                                    type: "POST"
 
-    if ((name_store != "") && (address_store != "") && (phone_1_store != "")) {
-            var parametros = {
-                "op": "create",
-                "name": name_store,
-                "address": address_store,
-                "phone_1": phone_1_store,
-                "phone_2": phone_2_store,
-                "email": email__store
+                                }).done(function (response) {
+                                    console.log(response);
+                                    if (response) {
+                                        window.location.href = "../html/success_register.html"
+                                    } else {
+                                        alert("Está email ya se ha registrado");
+                                    }
+
+                                }).fail(function () {
+                                    alert("error");
+                                });
+
+                            } else {
+                                alert("Las claves no coinciden");
+                            }
+                        } else {
+                            alert("Ingrese un correo electrónico válido");
+                        }
+                    } else {
+                        alert("La clave debe tener mínimo 8 carácteres");
+                    }
+                } else {
+                    alert("La clave debe contener al menos un número");
+                }
+            } else {
+                alert("La clave debe contener al menos una letra mayúscula");
             }
-            $.ajax({
-                data: parametros,
-                url: "../../StoreS",
-                type: "POST"
-
-            }).done(function (response) {
-                console.log(response); 
-                if(response){
-                    window.location.href = "../html/register_admin.html"
-                }else{
-                    alert("Está tienda ya se ha registrado");
-                }              
-
-            }).fail(function () {
-                alert("error");
-            });
-    }else{
+        } else {
+            alert("La clave debe contener al menos una letra minúscula");
+        }
+    } else {
         alert("Debe llenar los campos obligatorios para poder continuar");
     }
 
 }
+
+
